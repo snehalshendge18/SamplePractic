a1cf29af4d12d68f7f376bef8bed9e0150ea94ec
poonam.d <snehalshendge18@gmail.com>
2021-12-23 12:12:13 +0530

    MessageMethod

---
+++ SimplePracticGit/Controllers/FirstController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplePracticGit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimplePracticGit.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Message()
+        {
+            var a = new SampleModel();
+                a.Msg = "Hello...........";
+            return View(a);
+        }
+    }
+}
